783abe6715dbe984abe1e142bfe740d7dc3f9804
Rydev <ryanjnewsom@gmail.com>
2018-06-16 14:22:14 -0600

    scaffolded and hooked up posts add component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,11 +2,11 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component {
     componentDidMount () {
-        this.props.fetchPosts()
+        this.props.fetchPosts();
       }
 
       renderPosts () {
@@ -22,7 +22,10 @@ class PostsIndex extends Component {
       render () {
         return (
           <div>
-            <div className='text-sm-right'>
+            <div className='text-xs-right'>
+              <Link className="btn btn-primary" to="/posts/new">
+                Add a Post
+              </Link>
             </div>
             <h3>Posts</h3>
             <ul className='list-group'>
@@ -37,4 +40,4 @@ function mapStateToProps (state) {
     return { posts: state.posts }
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
